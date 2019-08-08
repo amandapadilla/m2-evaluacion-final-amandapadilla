c5966982188efafa9a21c83fc2ed8aa3eb8923c4
amandapadilla <amandapadillasantiago@gmail.com>
2019-08-09 00:18:43 +0200

    test print results

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,14 +1,14 @@
 "use strict";
 
 // RECUERDA HACER console.log(); para probar las funciones
-//debugger;
+debugger;
 
 //Almacenar constantes
 const inputSearch = document.querySelector(".js-inputSearch");
 const searchSubmit = document.querySelector(".js-searchSubmit");
 let data;
 let showCards;
-
+debugger;
 //Escuchar click de búsqueda
 
 //Leer datos de búsqueda
@@ -16,22 +16,21 @@ const formatInputSearchValue = () => {
   const inputSearchValue = inputSearch.value;
   return inputSearchValue.toLowerCase();
 };
-
+debugger;
 //Traer datos del servidor ¿Es posible traer con la URL sólo los datos que queremos?
-const getDataFromServer = ev => {
+const getDataFromServer = async ev => {
   event.preventDefault();
   const searchUrl = `http://api.tvmaze.com/search/shows?q=${formatInputSearchValue()}`;
-  return fetch(searchUrl) //probar con serie girls, pero se debe modificar la url para traer sólo lo que queremos. ¿Como programo que la última palabra la tome del input?
-    .then(response => response.json())
-    .then(data => {
-      console.log( data);
-      data = formatServerData(data);
-      //   saveDataInShowCards(data);
-      //   paintShowCards();
-    });
+  const response = await fetch(searchUrl) //probar con serie girls, pero se debe modificar la url para traer sólo lo que queremos. ¿Como programo que la última palabra la tome del input?
+    ;
+  const data = await response.json();
+  data = formatServerData(data);
+  saveDataInShowCards(data);
+  paintShowCards();
+  // listenShowCards();
 };
 searchSubmit.addEventListener("click", getDataFromServer);
-
+debugger;
 //Formatear datos (desde JSON seleccionar sólo nombre y portada)
 const formatServerData = data => {
     result =[];
@@ -42,37 +41,39 @@ const formatServerData = data => {
         });    
   return result;
 };
-
+debugger;
 //Recoger datos de búsqueda (nombre, portada)
 const saveDataInShowCards = (data) => {
-    showCards = data;
+  showCards = data;
 };
-//Pintar resultados en display
-
+//Pintar resultados en display --- REHACER CÓDIGO
+debugger;
 const paintShowCards = () => {
-    const displayCardsList = document.querySelector('.js-resultList');
-    for (let cardIndex = 0; cardIndex < showCards.length; cardIndex++) {
-
-
-
-
-
-
-
-
-
-      htmlCode += `<li class="result-item" ${getFavoriteClassName(cardIndex)} js-palette" data-index="${paletteIndex}">`;
-      htmlCode += `<p class="palettes__name">${palettes[paletteIndex].name}</p>`;
-      htmlCode += '<ul class="palettes__colors">';
-      for (const color of palettes[paletteIndex].colors) {
-        htmlCode += `<li class="palettes__color" style="background-color: #${color}"></li>`;
-      }
-      htmlCode += '</ul>';
-      htmlCode += '</li>';
+  const showCardsList = document.querySelector('.js-displayResults');
+  let htmlCode = '';
+  for (let cardIndex = 0; cardIndex < showCards.length; cardIndex++) {
+    htmlcode += `<li class="result-item" ${getFavoriteByClass(cardIndex)} js-showCard" data-index="${cardIndex}">`;
+    htmlCode += `<p class="showCards__name">${showCards[cardIndex].name}</p>`;
+    htmlCode += `<p class="showCards__image">${showCards[cardIndex].image}</p>`;
+    htmlCode += '<ul class="result-list">';
+    // for (const cardsList of palettes[paletteIndex].colors) {
+    //     htmlCode += `<li class="palettes__color" style="background-color: #${color}"></li>`;
+    //   }
+    //   htmlCode += '</ul>';
+    //   htmlCode += '</li>';
     }
     listElement.innerHTML = htmlCode;
-    console.log('Paint palettes form `palettes` array into DOM >>> Palettes:', palettes, 'Favorites:', favorites);
+    // console.log('Paint palettes form `palettes` array into DOM >>> Palettes:', palettes, 'Favorites:', favorites);
   };
+  debugger;
+  const getFavoriteByClass = cardIndex => {
+    if (isFavoriteShow(cardIndex)) {
+      return 'showCards__item--favorite';
+    } else {
+      return '';
+    }
+  };
+  debugger;
 ////////////////////////////////////////////
 
 //Escuchar click sobre tarjeta de serie
