94cffac9ac791ee644a6ec00deb7cad4f3756da8
amandapadilla <amandapadillasantiago@gmail.com>
2019-08-09 11:02:41 +0200

    fix fetch server + add new list element to DOM

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,13 +1,14 @@
 "use strict";
 
+
 // RECUERDA HACER console.log(); para probar las funciones
 // debugger;
 
 //Almacenar constantes
 const inputSearch = document.querySelector(".js-inputSearch");
 const searchSubmit = document.querySelector(".js-searchSubmit");
+let showCards = [];
 let data;
-let showCards;
 
 //Escuchar click de búsqueda
 
@@ -18,70 +19,53 @@ const formatInputSearchValue = () => {
 };
 
 //Traer datos del servidor ¿Es posible traer con la URL sólo los datos que queremos?
-const getDataFromServer = ev => {
+const getDataFromServer = (ev) => {
   event.preventDefault();
   const searchUrl = `http://api.tvmaze.com/search/shows?q=${formatInputSearchValue()}`;
   return fetch(searchUrl)
     .then (response => response.json())
     .then (data => {
-    console.log (data);
-    formatServerData(data);
-    saveDataInShowCards(data);
-    paintShowCards();
-    listenShowCards();
+      console.log ('Respuesta', data);
+      data = formatServerData(data);
+      saveDataInShowCards(data);
+      paintShowCards();
+      listenShowCards();
     });
-  };
+};
 
 searchSubmit.addEventListener("click", getDataFromServer);
 
 //Formatear datos (desde JSON seleccionar sólo nombre y portada)
 const formatServerData = data => {
-    result =[];
-  for (let i=0;i<data.length;x++) {
+  result =[];
+  for (let i=0;i<data.length;i++) {
     result.push({
-        name: data.name,
-        image: data.image
-        });    
-  return result};
+      name: data.name,
+      image: data.image
+    });    
+    return result}
 };
-debugger;
+
 //Recoger datos de búsqueda (nombre, portada)
 const saveDataInShowCards = (data) => {
   showCards = data;
 };
 //Pintar resultados en display --- REHACER CÓDIGO
-debugger;
-const paintShowCards = () => {
-  const showCardsList = document.querySelector('.js-displayResults');
-  let htmlCode = '';
-  for (let cardIndex = 0; cardIndex < showCards.length; cardIndex++) {
-    htmlcode += `<li class="result-item" ${getFavoriteByClass(cardIndex)} js-showCard" data-index="${cardIndex}">`;
-    htmlCode += `<p class="showCards__name">${showCards[cardIndex].name}</p>`;
-    htmlCode += `<p class="showCards__image">${showCards[cardIndex].image}</p>`;
-    htmlCode += '<ul class="result-list">';
-    // for (const cardsList of palettes[paletteIndex].colors) {
-    //     htmlCode += `<li class="palettes__color" style="background-color: #${color}"></li>`;
-    //   }
-    //   htmlCode += '</ul>';
-    //   htmlCode += '</li>';
-    }
-    listElement.innerHTML = htmlCode;
-    // console.log('Paint palettes form `palettes` array into DOM >>> Palettes:', palettes, 'Favorites:', favorites);
-  };
-  debugger;
-  const getFavoriteByClass = cardIndex => {
-    if (isFavoriteShow(cardIndex)) {
-      return 'showCards__item--favorite';
-    } else {
-      return '';
-    }
-  };
-  debugger;
+
+const newShowCard = document.createElement('li');
+const newShowCardContent = document.createTextNode(showCards);
+newShowCard.appendChild(newShowCardContent);
+const showCardsList = document.querySelector('.js-displayResults');
+showCardsList.appendChild(newShowCard);
+  
+//¿Cómo hago el bucle para que me pinte todos los resultados?
+
+
 ////////////////////////////////////////////
 
 //Escuchar click sobre tarjeta de serie
 
-//Leer datos seleccionados
+//Leer datos seleccionados ¿Is favorite? true/false
 
 //Cambiar estilo de los datos seleccionados
 
@@ -96,4 +80,4 @@ const paintShowCards = () => {
 ////////////////////////////////////////////
 
 //OPCIONAL Crear funcionalidad 'Quitar de Favoritos'
-//OPCIONAL 
+//OPCIONAL Reset Button
