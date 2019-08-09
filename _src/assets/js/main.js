7fa933c925000f5387119fa232f6912adffcc597
amandapadilla <amandapadillasantiago@gmail.com>
2019-08-09 12:04:33 +0200

    little changes - change pc

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -22,10 +22,11 @@ const formatInputSearchValue = () => {
 const getDataFromServer = (ev) => {
   event.preventDefault();
   const searchUrl = `http://api.tvmaze.com/search/shows?q=${formatInputSearchValue()}`;
+  
   return fetch(searchUrl)
     .then (response => response.json())
     .then (data => {
-      console.log ('Respuesta', data);
+      
       data = formatServerData(data);
       saveDataInShowCards(data);
       paintShowCards();
@@ -37,13 +38,15 @@ searchSubmit.addEventListener("click", getDataFromServer);
 
 //Formatear datos (desde JSON seleccionar sólo nombre y portada)
 const formatServerData = data => {
-  result =[];
+  const result =[];
   for (let i=0;i<data.length;i++) {
     result.push({
       name: data.name,
       image: data.image
-    });    
-    return result}
+    });
+  };
+  return result;
+  console.log ('Respuesta', data);
 };
 
 //Recoger datos de búsqueda (nombre, portada)
@@ -59,17 +62,56 @@ const showCardsList = document.querySelector('.js-displayResults');
 showCardsList.appendChild(newShowCard);
   
 //¿Cómo hago el bucle para que me pinte todos los resultados?
-
+const addFavoriteClassToNewShowCard = showCardsIndex 
 
 ////////////////////////////////////////////
 
 //Escuchar click sobre tarjeta de serie
+const listenClickOnShowCards = () => {
+  console.log('Listen click on new showCards DOM elements');
+  const paletteElements = document.querySelectorAll('.js-palette');
+  for (const paletteElement of paletteElements) {
+    paletteElement.addEventListener('click', handleClick);
+  }
+};
+const handleClick = ev => {
+  console.log('Handle click on a palette DOM element');
+  const paletteIndex = getClickedPalette(ev);
+  if (isFavoritePalette(paletteIndex)) {
+    removeFavorite(paletteIndex);
+  } else {
+    addFavorite(paletteIndex);
+  }
+  paintPalettes();
+};
 
 //Leer datos seleccionados ¿Is favorite? true/false
+const getClickedPalette = ev => {
+  const clickedPaletteIndex = 1;
+  console.log('Get clicked palette from event and return the clicked palette index >>> Clicked palette:', clickedPaletteIndex);
+  return clickedPaletteIndex;
+};
+const isFavoritePalette = paletteIndex => {
+  if (paletteIndex === 1) {
+    console.log(`Check if paletteIndex ${paletteIndex} it is favorite >>>`, true);
+    return true;
+  } else {
+    console.log(`Check if paletteIndex ${paletteIndex} it is not favorite >>>`, false);
+    return false;
+  }
+};
 
 //Cambiar estilo de los datos seleccionados
 
+// const addStylesToSelectedShowCard = () => {
+//   if (isFavoriteShowCard) 
+// };
+
 //Guardar datos seleccionados en un array de FAVORITOS
+const addFavorite = paletteIndex => {
+  favorites.push(paletteIndex);
+  console.log('Add paletteIndex to `favorites` array >>> Favorites:', favorites);
+};
 
 //Pintar en FAVORITOS (temporizador 1s) para permitir ver el cambio de la tarjeta
 
@@ -80,4 +122,8 @@ showCardsList.appendChild(newShowCard);
 ////////////////////////////////////////////
 
 //OPCIONAL Crear funcionalidad 'Quitar de Favoritos'
+const removeFavorite = paletteIndex => {
+  console.log('Remove paletteIndex from `favorites` array >>> Favorites:', favorites);
+};
+
 //OPCIONAL Reset Button
